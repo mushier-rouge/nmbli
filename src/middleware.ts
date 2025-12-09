d99e7485d7da67d9e5716cb85f43513e016194ea
s <s@MacBook-Pro.local>
2025-12-08 19:44:09 -0800

    Fix middleware regex syntax

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,6 @@ export const config = {
          * - /api/auth/callback (auth callback)
          * Feel free to modify this pattern to include more paths.
          */
-        '/((?!_next/static|_next/image|favicon.ico |api/auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|api/auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
